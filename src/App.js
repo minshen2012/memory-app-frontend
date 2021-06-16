6adaa65edd50b1a028825e74693d8c22a3b1a9c9
mason <kuangminshen@gmail.com>
2021-06-16 10:06:43 +1000

    create Posts and Form components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+
 const App = () => {
   return (
-    <div>
-      <h1>Appppppp</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography varaint="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="sapce-between"
+            alignItems="stretch"
+            sapcing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
