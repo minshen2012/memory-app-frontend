d744c6b450ee377f67695f34c7da6ae039ac3de5
mason <kuangminshen@gmail.com>
2021-06-17 21:25:51 +1000

    make create post working and display

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -8,12 +9,21 @@ const Posts = () => {
   const classes = useStyles();
 
   console.log(posts);
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
